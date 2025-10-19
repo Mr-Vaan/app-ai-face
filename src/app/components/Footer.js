549464754f425af5079d52a0ed0e71adb7a7e792
ivandhebel <ivandhebel@gmail.com>
2025-10-19 11:45:29 +0700

    Initial Setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className='text-sm text-center italic'>
+      <p>&copy; 2025 AI Face. All Right Reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
