9c78ffadaed7aae158fed1716abff62e408c22ed
ivandhebel <ivandhebel@gmail.com>
2025-10-19 20:39:07 +0700

    Ubah file src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
